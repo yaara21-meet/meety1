ab13f692c57a4e997e0352745e99c88e4b739323
yaara21-meet <52988345+yaara21-meet@users.noreply.github.com>
2020-01-08 12:24:32 -0500

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,37 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+var seven = 7;
+var eight = 8;
+var nine = 9;
+
+
+alert("hey there, we are going to play a tic tac toe game!\n this is the bours: \n"+one+" "+two+" "+three+"\n"+four+" "+five+" "+six+"\n"+seven+" "+eight+" "+nine);
+var firPlayer = prompt("please enter the name of the first player");
+var srcPlayer = prompt("please enter the name of the second player");
+
+var userNum =  alert(firPlayer + ", plese enter the number of the plase oyu want to mark\n"+one+" "+two+" "+three+"\n"+four+" "+five+" "+six+"\n"+seven+" "+eight+" "+nine);
+
+swich(userNum)
+{
+	case 1:
+		alert(secPlayer + ", plese enter the number of the plase oyu want to mark\n"two+" "+three+"\n"+four+" "+five+" "+six+"\n"+seven+" "+eight+" "+nine);
+	break;
+}
+
+
+
+
+var birthYear = parseInt(prompt("what is your birth year?"));
+console.log(birthYear);//birth day of user
+
+var luckyNum = parseInt(prompt("what is your lucky number?"));
+console.log(luckyNum);// lucky number of user\
+
+var favFruit = "apples";
+var favSubject = "math";
+
+alert(birthYear + favSubject);
